dokipen/trac

commit 90ab1ce0be0dc7ca7e1a83794ed961781df52104

InterTrac branch merged into the trunk. See log:sandbox/intertrac@2831 for the change history.
git-svn-id: https://svn.edgewall.org/repos/trac/trunk@2839 af82e41b-90c4-0310-8c96-b1721e28e2e2

trac/wiki/formatter.py
@@ -27,10 +27,11 @@ except ImportError:
     from StringIO import StringIO
 
 from trac import util
+from trac.core import *
 from trac.mimeview import *
-from trac.wiki.api import WikiSystem
+from trac.wiki.api import WikiSystem, IWikiChangeListener, IWikiMacroProvider
 
-__all__ = ['wiki_to_html', 'wiki_to_oneliner', 'wiki_to_outline']
+__all__ = ['wiki_to_html', 'wiki_to_oneliner', 'wiki_to_outline', 'Formatter' ]
 
 
 def system_message(msg, text):
@@ -131,6 +132,7 @@ class Formatter(object):
     INLINE_TOKEN = "`"
 
     LINK_SCHEME = r"[\w.+-]+" # as per RFC 2396
+    INTERTRAC_SCHEME = r"[a-zA-Z.+-]+?" # no digits (support for shorthand links)
 
     QUOTED_STRING = r"'[^']+'|\"[^\"]+\""
 
@@ -289,13 +291,65 @@ class Formatter(object):
             return self._make_link(ns, target, match, label)
 
     def _make_link(self, ns, target, match, label):
+        # check first for an alias defined in trac.ini
+        ns = self.env.config.get('intertrac', ns.upper(), ns)
         if ns in self.link_resolvers:
             return self.link_resolvers[ns](self, ns, target,
                                            util.escape(label, False))
         elif target.startswith('//') or ns == "mailto":
             return self._make_ext_link(ns+':'+target, label)
         else:
-            return util.escape(match)
+            return self._make_intertrac_link(ns, target, label) or \
+                   self._make_interwiki_link(ns, target, label) or \
+                   match
+
+    def _make_intertrac_link(self, ns, target, label):
+        if self.env.siblings.has_key(ns):
+            sibling = self.env.siblings[ns]
+            # The following is currently needed because env.href is set
+            # in trac.web.main.dispatch_request: for an environment which
+            # has not yet been queried by a client, .href is not defined.
+            if not hasattr(sibling, 'href'):
+                from trac.web.href import Href
+                def xchg_base(base):
+                    return '/'.join(base.split('/')[:-1] + [ns])
+                sibling.href = Href(xchg_base(self.env.href.base))
+                sibling.abs_href = Href(xchg_base(self.env.abs_href.base))
+            # EOKludge
+            ref = wiki_to_oneliner(target, sibling)
+            return ref.replace('>%s' % target, '>%s' % label)
+        url = self.env.config.get('intertrac', ns.upper()+'.url')
+        if url:
+            name = self.env.config.get('intertrac', ns.upper()+'.title',
+                                       'Trac project %s' % ns)
+            sep = target.find(':')
+            if sep != -1:
+                url = '%s/%s/%s' % (url, target[:sep], target[sep+1:])
+            else: 
+                url = '%s/search?q=%s' % (url, urllib.quote_plus(target))
+            return self._make_ext_link(url, label, '%s in %s' % (target, name))
+        else:
+            return None
+
+    def shorthand_intertrac_helper(self, ns, target, label, fullmatch):
+        if fullmatch: # short form
+            it_group = fullmatch.group('it_%s' % ns)
+            if it_group:
+                alias = it_group.strip()
+                intertrac = self.env.config.get('intertrac', alias.upper(),
+                                                alias)
+                target = '%s:%s' % (ns, target[len(it_group):])
+                return self._make_intertrac_link(intertrac, target, label) or \
+                       label
+        return None
+
+    def _make_interwiki_link(self, ns, target, label):
+        interwiki = InterWikiMap(self.env)
+        if interwiki.has_key(ns):
+            url, title = interwiki.url(ns, target)
+            return self._make_ext_link(url, label, '%s in %s' % (target, title))
+        else:
+            return None
 
     def _make_ext_link(self, url, text, title=''):
         url = util.escape(url)
@@ -754,3 +808,98 @@ def wiki_to_outline(wikitext, env, db=None, absurls=0, max_depth=None,
     OutlineFormatter(env, absurls, db).format(wikitext, out, max_depth,
                                               min_depth)
     return util.Markup(out.getvalue())
+
+
+# -- InterWiki support
+
+class InterWikiMap(Component):
+
+    implements(IWikiChangeListener, IWikiMacroProvider)
+
+    _page_name = 'InterMapTxt'
+    _interwiki_re = re.compile(r"(\w+)[ \t]+([^ \t]+)(?:[ \t]+#(.*))?",
+                               re.UNICODE)
+    _argspec_re = re.compile(r"\$\d")
+
+    def __init__(self):
+        self._interwiki_map = None
+        # This dictionary maps upper-cased namespaces
+        # to (namespace, prefix, title) values
+
+    def has_key(self, ns):
+        if not self._interwiki_map:
+            self._update()
+        return self._interwiki_map.has_key(ns.upper())
+
+    def url(self, ns, target):
+        ns, url, title = self._interwiki_map[ns.upper()]
+        args = target.split(':')
+        def setarg(match):
+            num = int(match.group()[1:])
+            return 0 < num <= len(args) and args[num-1] or ''
+        url_with_args = re.sub(InterWikiMap._argspec_re, setarg, url)
+        if url_with_args == url: 
+            return url + target, title
+        else:
+            return url_with_args, title
+
+    # IWikiChangeListener methods
+
+    def wiki_page_added(self, page):
+        if page.name == InterWikiMap._page_name:
+            self._update()
+
+    def wiki_page_changed(self, page, version, t, comment, author, ipnr):
+        if page.name == InterWikiMap._page_name:
+            self._update()
+
+    def wiki_page_deleted(self, page):
+        if page.name == InterWikiMap._page_name:
+            self._interwiki_map.clear()
+
+    def _update(self):
+        from trac.wiki.model import WikiPage
+        self._interwiki_map = {}
+        content = WikiPage(self.env, InterWikiMap._page_name).text
+        in_map = False
+        for line in content.split('\n'):
+            if in_map:
+                if line.startswith('----'):
+                    in_map = False
+                else:
+                    m = re.match(InterWikiMap._interwiki_re, line)
+                    if m:
+                        prefix, url, title = m.groups()
+                        url = url.strip()
+                        title = title and title.strip() or prefix
+                        self._interwiki_map[prefix.upper()] = (prefix, url,
+                                                               title)
+            elif line.startswith('----'):
+                in_map = True
+
+    # IWikiMacroProvider
+
+    def get_macros(self):
+        yield 'InterWiki'
+
+    def get_macro_description(self, name): 
+        return "Provide a description list for the known InterWiki prefixes."
+
+    def render_macro(self, req, name, content):
+        if not self._interwiki_map:
+            self._update()
+        keys = self._interwiki_map.keys()
+        keys.sort()
+        buf = StringIO()
+        buf.write('<table><tr><th>Prefix</th><td>Site</td></tr>\n')
+        for k in keys:
+            prefix, url, title = self._interwiki_map[k]
+            shortened_url = url and url[:-1]
+            description = title == prefix and shortened_url or title
+            buf.write('<tr>\n' +
+                      ('<td><a href="%sRecentChanges">%s</a></td>'
+                       '<td><a href="%s">%s</a></td>\n') \
+                      % (url, prefix, shortened_url, description) +
+                      '</tr>\n')
+        buf.write('</table>\n')
+        return buf.getvalue()

trac/wiki/tests/formatter.py
@@ -54,8 +54,13 @@ class WikiTestCase(unittest.TestCase):
                 self.config = Configuration(None)
                 self.href = Href('/')
                 self.abs_href = Href('http://www.example.com/')
-                self._wiki_pages = {}
                 self.path = ''
+                # -- intertrac support
+                self.siblings = {}
+                self.config.set('intertrac', 'trac.title', "Trac's Trac")
+                self.config.set('intertrac', 'trac.url',
+                                "http://projects.edgewall.com/trac")
+                self.config.set('intertrac', 't', 'trac')
             def component_activated(self, component):
                 component.env = self
                 component.config = self.config

trac/env.py
@@ -75,6 +75,7 @@ class Environment(Component, ComponentManager):
         ComponentManager.__init__(self)
 
         self.path = path
+        self.siblings = {}
         self.load_config()
         self.setup_log() 
 

trac/ticket/api.py
@@ -17,7 +17,7 @@
 from trac import util
 from trac.core import *
 from trac.perm import IPermissionRequestor
-from trac.wiki import IWikiSyntaxProvider
+from trac.wiki import IWikiSyntaxProvider, Formatter
 from trac.Search import ISearchSource, query_to_sql, shorten_result
 
 
@@ -138,10 +138,18 @@ class TicketSystem(Component):
                 ('ticket', self._format_link)]
 
     def get_wiki_syntax(self):
-        yield (r"!?(?<!&)#\d+", # #123 but not &#123; (HTML entity)
-               lambda x, y, z: self._format_link(x, 'ticket', y[1:], y))
+        yield (
+            # matches #... but not &#... (HTML entity)
+            r"!?(?<!&)#"
+            # optional intertrac shorthand #T... + digits
+            r"(?P<it_ticket>%s)?\d+" % Formatter.INTERTRAC_SCHEME,
+            lambda x, y, z: self._format_link(x, 'ticket', y[1:], y, z))
 
-    def _format_link(self, formatter, ns, target, label):
+    def _format_link(self, formatter, ns, target, label, fullmatch=None):
+        intertrac = formatter.shorthand_intertrac_helper(ns, target, label,
+                                                         fullmatch)
+        if intertrac:
+            return intertrac
         cursor = formatter.db.cursor()
         cursor.execute("SELECT summary,status FROM ticket WHERE id=%s",
                        (target,))

trac/ticket/report.py
@@ -23,7 +23,7 @@ from trac.core import *
 from trac.perm import IPermissionRequestor
 from trac.web import IRequestHandler
 from trac.web.chrome import add_link, add_stylesheet, INavigationContributor
-from trac.wiki import wiki_to_html, IWikiSyntaxProvider
+from trac.wiki import wiki_to_html, IWikiSyntaxProvider, Formatter
 
 
 dynvars_re = re.compile('\$([A-Z]+)')
@@ -499,9 +499,14 @@ class ReportModule(Component):
         yield ('report', self._format_link)
 
     def get_wiki_syntax(self):
-        yield (r"!?\{\d+\}", lambda x, y, z: self._format_link(x, 'report', y[1:-1], y))
+        yield (r"!?\{(?P<it_report>%s\s*)?\d+\}" % Formatter.INTERTRAC_SCHEME,
+               lambda x, y, z: self._format_link(x, 'report', y[1:-1], y, z))
 
-    def _format_link(self, formatter, ns, target, label):
+    def _format_link(self, formatter, ns, target, label, fullmatch=None):
+        intertrac = formatter.shorthand_intertrac_helper(ns, target, label,
+                                                         fullmatch)
+        if intertrac:
+            return intertrac
         report, args = target, ''
         if '?' in target:
             report, args = target.split('?')

trac/versioncontrol/web_ui/changeset.py
@@ -34,7 +34,8 @@ from trac.versioncontrol.diff import get_diff_options, hdf_diff, unified_diff
 from trac.versioncontrol.svn_authz import SubversionAuthorizer
 from trac.web import IRequestHandler
 from trac.web.chrome import INavigationContributor, add_link, add_stylesheet
-from trac.wiki import wiki_to_html, wiki_to_oneliner, IWikiSyntaxProvider
+from trac.wiki import wiki_to_html, wiki_to_oneliner, IWikiSyntaxProvider, \
+                      Formatter
 
 
 class DiffArgs(dict):
@@ -580,11 +581,17 @@ class ChangesetModule(Component):
     # IWikiSyntaxProvider methods
 
     def get_wiki_syntax(self):
-        yield (r"!?\[\d+(?:/[^\]]*)?\]|(?:\b|!)r\d+\b(?!:\d)",
-               lambda x, y, z:
-               self._format_changeset_link(x, 'changeset',
-                                           y[0] == 'r' and y[1:] or y[1:-1],
-                                           y, z))
+        yield (
+            # [...] form: start with optional intertrac: [T... or [trac ... 
+            r"!?\[(?P<it_changeset>%s\s*)?" % Formatter.INTERTRAC_SCHEME +
+            #  digits + optional path for the restricted changeset
+            r"\d+(?:/[^\]]*)?\]|"   
+            # r... form: allow r1 but not r1:2 (handled by the log syntax)
+            r"(?:\b|!)r\d+\b(?!:\d)",
+            lambda x, y, z:
+            self._format_changeset_link(x, 'changeset',
+                                        y[0] == 'r' and y[1:] or y[1:-1],
+                                        y, z))
 
     def get_link_resolvers(self):
         yield ('changeset', self._format_changeset_link)
@@ -592,6 +599,10 @@ class ChangesetModule(Component):
 
     def _format_changeset_link(self, formatter, ns, chgset, label,
                                fullmatch=None):
+        intertrac = formatter.shorthand_intertrac_helper(ns, chgset, label,
+                                                         fullmatch)
+        if intertrac:
+            return intertrac
         sep = chgset.find('/')
         if sep > 0:
             rev, path = chgset[:sep], chgset[sep:]

trac/web/main.py
@@ -17,6 +17,8 @@
 #         Matthew Good <trac@matt-good.net>
 
 import os
+import sys
+import dircache
 
 from trac.core import *
 from trac.env import open_environment
@@ -279,24 +281,17 @@ def send_project_index(req, options, env_paths=None):
    <a href="<?cs var:project.href ?>" title="<?cs var:project.description ?>">
     <?cs var:project.name ?></a><?cs
   else ?>
-   <small><?cs var:project.name ?>: <em>Error</em> <br />
-   (<?cs var:project.description ?>)</small><?cs
+   <?cs var:project.name ?>
+   <small><em><?cs var:project.description ?></em></small><?cs
   /if ?>
   </li><?cs
  /each ?></ul></body>
 </html>''')
 
-    if not env_paths and 'TRAC_ENV_PARENT_DIR' in options:
-        dir = options['TRAC_ENV_PARENT_DIR']
-        env_paths = [os.path.join(dir, f) for f in os.listdir(dir)]
-
     href = Href(req.idx_location)
     try:
         projects = []
-        for env_path in env_paths:
-            if not os.path.isdir(env_path):
-                continue
-            env_dir, project = os.path.split(env_path)
+        for project, env_path in get_projects(options, env_paths).items():
             try:
                 env = _open_environment(env_path)
                 proj = {
@@ -322,7 +317,7 @@ def get_environment(req, options, threaded=True):
     elif 'TRAC_ENV_PARENT_DIR' in options:
         env_parent_dir = options['TRAC_ENV_PARENT_DIR']
         env_name = req.cgi_location.split('/')[-1]
-        env_path = os.path.join(env_parent_dir, env_name)
+        env_path = os.path.join(os.path.normpath(env_parent_dir), env_name)
         if not len(env_name) or not os.path.exists(env_path):
             return None
     else:
@@ -333,3 +328,53 @@ def get_environment(req, options, threaded=True):
               'the Trac environment(s).'
 
     return _open_environment(env_path, threaded)
+
+
+def get_projects(options, env_paths, warn=False):
+    """Retrieve canonical project to environment path mapping.
+
+    The environments may not be all valid environments, though,
+    but they are serious candidates...
+    """
+    env_paths = env_paths or []
+    if 'TRAC_ENV_PARENT_DIR' in options:
+        env_parent_dir = os.path.normpath(options['TRAC_ENV_PARENT_DIR'])
+        if env_parent_dir:
+            paths = dircache.listdir(env_parent_dir)[:]
+            dircache.annotate(env_parent_dir, paths)
+            env_paths += [os.path.join(env_parent_dir, project) \
+                          for project in paths if project[-1] == '/']
+    projects = {}
+    for env_path in env_paths:
+        env_path = os.path.normpath(env_path)
+        if not os.path.isdir(env_path):
+            continue
+        project = os.path.split(env_path)[1]
+        if project in projects:
+            if warn:
+                print >>sys.stderr,('Warning: Ignoring project "%s" since ' \
+                                    'it conflicts with project "%s"' \
+                                    % (env_path, projects[project]))
+        else:
+            projects[project] = env_path
+    return projects
+
+def setup_sibling_environments(options, env_paths=None):
+    """Make each of the given environment know all the others.
+
+    The environments can be specified by a list of `env_paths`
+    and also from the `TRAC_ENV_PARENT_DIR` options.
+    Return a mapping of project names to environment paths.
+    """
+    siblings = {}
+    projects = get_projects(options, env_paths, warn=True)
+    options = options.copy()
+    for project, env_path in projects.items():
+        options['TRAC_ENV'] = env_path
+        try:
+            siblings[project] = get_environment(None, options)
+        except (TracError, IOError):
+            pass
+    for env in siblings.values():
+        env.siblings = siblings
+    return projects

trac/web/modpython_frontend.py
@@ -31,6 +31,7 @@ from mod_python import apache, util
 from trac.util import http_date
 from trac.web.api import Request, RequestDone
 from trac.web.main import dispatch_request, get_environment, \
+                          setup_sibling_environments, \
                           send_pretty_error, send_project_index
 
 
@@ -196,6 +197,7 @@ def handler(req):
             ('TracEnvParentDir', 'TRAC_ENV_PARENT_DIR'),
             ('TracEnvIndexTemplate', 'TRAC_ENV_INDEX_TEMPLATE'),
             ('TracTemplateVars', 'TRAC_TEMPLATE_VARS'))
+    setup_sibling_environments(project_opts)
     env = get_environment(mpr, project_opts)
     if not env:
         send_project_index(mpr, project_opts)

commit 20340cf7d02a262b9933f17622ef21ef2db82663

Wiki link formatter: missing escape for links look-alike
This fixes a bug introduced in r2839, which didn't take into account r2644.

Closes #7332, many thanks to Nathan Collins for spotting this long standing issue!


git-svn-id: http://svn.edgewall.org/repos/trac/branches/0.11-stable@7205 af82e41b-90c4-0310-8c96-b1721e28e2e2

trac/wiki/formatter.py
@@ -395,7 +395,7 @@ class Formatter(object):
                 label = ns+':'+label
             return self._make_intertrac_link(ns, target, label) or \
                    self._make_interwiki_link(ns, target, label) or \
-                   match
+                   escape(match)
 
     def _make_intertrac_link(self, ns, target, label):
         intertrac = self.env.config['intertrac']